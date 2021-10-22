f977d1457b72a3add3459b4acf780ccbbbadef11
hanasmile <hanasmile12@gmail.com>
2021-10-22 17:55:32 +0900

    Add: email unique, created, updated value

--- students/hwayeon/users/models.py
+++ students/hwayeon/users/models.py
@@ -3,9 +3,11 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     username = models.CharField(max_length=50)
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
     password = models.CharField(max_length=200)
     phone_number = models.CharField(max_length=120)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'users'
