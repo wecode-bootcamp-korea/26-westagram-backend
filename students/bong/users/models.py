aabc7b6179c03aeb096801ef905f4f4d7a884a32
kimbongcheol <solarrrrr1010@gmail.com>
2021-10-22 17:58:42 +0900

    models2_repair2

--- students/bong/users/models.py
+++ students/bong/users/models.py
@@ -7,7 +7,7 @@ class User(models.Model):
     email = models.EmailField(max_length=200, unique=True)
     password = models.CharField(max_length=200)
     phone_num = models.CharField(max_length=13)
-    related_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
     class Meta:
