aed9ebc5ff2fbe7cf88c2323a81d8ca8d00af25f
1119 pdh <1119pdh@gmail.com>
2021-10-22 18:02:41 +0900

    project models

--- students/parkdaehyun/users/models.py
+++ students/parkdaehyun/users/models.py
@@ -4,10 +4,13 @@ from django.db import models
 
 class User(models.Model):
     name                 = models.CharField(max_length=100)
-    email                = models.EmailField(max_length=100)
+    email                = models.EmailField(max_length=100, unique=True)
     password             = models.CharField(max_length=100)
     telephone            = models.CharField(max_length=100)
     personal_information = models.TextField(max_length=2000)
+    created_at           = models.DateTimeField(auto_now_add=True)
+    updated_at           = models.DateTimeField(auto_now=True)
+
 
     class Meta:
         db_table = 'users'
