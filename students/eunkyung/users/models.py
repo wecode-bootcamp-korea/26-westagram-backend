bca36c7c6a1f2020ecd0836a35dbcc528770f9fd
권은경 <kwon-eunkyung@gwon-eungyeong-ui-MacBookAir.local>
2021-10-22 16:36:49 +0900

    CREAT:models.py 수정

--- students/eunkyung/users/models.py
+++ students/eunkyung/users/models.py
@@ -5,9 +5,9 @@ from django.db import models
 class User(models.Model):
     name         = models.CharField(max_length=45)
     email        = models.CharField(max_length=45, unique=True)
-    password     = models.CharField(max_length=45)
-    phone_number = models.PositiveIntegerField(default=0)
-    address      = models.TextField ()
+    password     = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=45)
+    address      = models.CharField(max_length=200)
 
     class Meta:
         db_table = 'users'
