4731d1455466dc7856dd82a64f1d38c3b9faffe3
Yonggeon Lee <yonggeonlee@Yonggeonui-MacBookAir.local>
2021-10-22 17:53:49 +0900

    Updated modeling contents_1

--- students/drageon/users/models.py
+++ students/drageon/users/models.py
@@ -4,11 +4,13 @@ from django.db import models
 
 class User(models.Model):
     name = models.CharField(max_length=50)
-    email = models.CharField(max_length=100)
-    password = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=200)
     contact = models.CharField(max_length=50)
     date_of_birth = models.DateField()
-    hobby = models.TextField()
+    hobby = models.CharField(max_length = 100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta : 
         db_table = 'users'
