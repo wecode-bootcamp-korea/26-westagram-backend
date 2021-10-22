4a0ac76a82f8d27d81f730dea9ace63b1f32b9e0
Minjeong Bak <doniminp@gmail.com>
2021-10-22 16:44:45 +0900

    modify: project models setting_2

--- students/minjeong/users/models.py
+++ students/minjeong/users/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=45)
     email = models.CharField(max_length=300, unique=True)
-    password = models.CharField(max_length=30)
+    password = models.CharField(max_length=200)
     phone_number = models.CharField(max_length=13)
     personal_info = models.TextField()
 
