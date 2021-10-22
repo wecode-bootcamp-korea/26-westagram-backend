742deb0d6d3a153c7f4ba6d968883b5728f10d17
Kyuuu827 <lck0827@gmail.com>
2021-10-22 13:22:24 +0900

    modified models attribute

--- students/chankyu/users/models.py
+++ students/chankyu/users/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    name = models.CharField(max_length=50)
-    email = models.CharField(max_length=50, unique=True)
-    password = models.CharField(max_length=30)
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=200)
     phone_number = models.CharField(max_length=30)
     age = models.PositiveIntegerField() 
     
