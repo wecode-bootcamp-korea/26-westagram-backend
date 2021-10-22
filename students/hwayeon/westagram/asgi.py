94ce71f183e6368d6896a79fa1b9977970b53e0c
hanasmile <hanasmile12@gmail.com>
2021-10-22 15:21:47 +0900

    Modify: settings.py 파일 내 누락된 내용 추가 및 그외 수정되지않은 앱 명칭 수정

--- students/hwayeon/westagram/asgi.py
+++ students/hwayeon/westagram/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'westargram.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'westagram.settings')
 
 application = get_asgi_application()
--- students/hwayeon/westagram/wsgi.py
+++ students/hwayeon/westagram/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'westargram.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'westagram.settings')
 
 application = get_wsgi_application()
