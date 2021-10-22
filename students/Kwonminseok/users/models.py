87e3c1fe7224e51964a4a602c2d908250042245a
knch1014 <knch1014@gmail.com>
2021-10-22 14:31:55 +0900

    [권민석] Mission 2: 모델링

---
+++ students/Kwonminseok/users/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+      name         = models.CharField(max_length=100)
+      email        = models.CharField(max_length=100, unique=True)
+      password     = models.CharField(max_length=200)
+      phone_number = models.CharField(max_length=50)
+      age          = models.PositiveIntegerField()
+
+      class Meta:
+            db_table = 'users'
