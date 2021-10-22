473b71a297eedca0400eb8a8823bd26fbb8d0a6b
Gyubs <gjrbqls95@naver.com>
2021-10-22 17:53:52 +0900

    gyubin - modeling(add:created, updated field)

--- students/gyubin/users/models.py
+++ students/gyubin/users/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
 class User(models.Model):
-	name      = models.CharField(max_length=100)
-	email     = models.CharField(max_length=100, unique=True)
-	password  = models.CharField(max_length=200)
-	contact   = models.CharField(max_length=100)
+    name      = models.CharField(max_length=100)
+    email     = models.CharField(max_length=100, unique=True)
+    password  = models.CharField(max_length=200)
+    contact   = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-	class Meta:
-		db_table = 'users'
+    class Meta:
+        db_table = 'users'
