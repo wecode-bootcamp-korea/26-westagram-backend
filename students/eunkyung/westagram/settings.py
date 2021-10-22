0c8bc46277b398a184248bbe4281f9421945e5c9
권은경 <kwon-eunkyung@gwon-eungyeong-ui-MacBookAir.local>
2021-10-22 14:24:27 +0900

    project initial setting-수정

--- students/eunkyung/westagram/settings.py
+++ students/eunkyung/westagram/settings.py
@@ -10,10 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib            import Path
-from my_settings     import DATABASES, SECRET_KEY
+from pathlib import Path
 
 import pymysql
+
+from my_settings import DATABASES, SECRET_KEY
 
 pymysql.install_as_MySQLdb()
 
