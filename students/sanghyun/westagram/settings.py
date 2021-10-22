ea2f3e8c79986b44e9859e79ec57227cc14593b2
gshduet <modis2199@gmail.com>
2021-10-22 21:11:34 +0900

    ADD : users 앱 생성 및 models class 작성

--- students/sanghyun/westagram/settings.py
+++ students/sanghyun/westagram/settings.py
@@ -1,5 +1,8 @@
-from pathlib import Path
-from my_settings import DATABASES, SECRET_KEY
+from pathlib        import Path
+from my_settings    import DATABASES, SECRET_KEY
+import pymysql
+
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +29,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders'
+    'corsheaders',
+    'users',
 ]
 
 MIDDLEWARE = [
