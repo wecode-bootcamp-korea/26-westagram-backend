de3825de8fd009603b4a71dace4a2ddec5207163
Gyubs <gjrbqls95@naver.com>
2021-10-22 16:53:51 +0900

    gyubin - modeling(add:email unique)

--- students/gyubin/users/models.py
+++ students/gyubin/users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class User(models.Model):
 	name      = models.CharField(max_length=100)
-	email     = models.CharField(max_length=100)
+	email     = models.CharField(max_length=100, unique=True)
 	password  = models.CharField(max_length=200)
 	contact   = models.CharField(max_length=100)
 
