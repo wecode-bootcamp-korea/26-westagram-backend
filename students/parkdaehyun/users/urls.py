16978385f7e43e3fdd3aa0c127dbda7d117b415d
1119 pdh <1119pdh@gmail.com>
2021-10-25 17:51:10 +0900

    project sign-up

--- students/parkdaehyun/users/urls.py
+++ students/parkdaehyun/users/urls.py
@@ -1,7 +1,6 @@
 from django.urls               import path
 from django.urls.resolvers import URLPattern
 from django.views.generic.base import View
-
 from .views                    import UserlistView
 
 
