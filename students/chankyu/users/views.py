f47afaeefa89bb79f7f22fe1be4be2ad7dccd9e3
Kyuuu827 <lck0827@gmail.com>
2021-10-25 21:20:11 +0900

    modified method of response

--- students/chankyu/users/views.py
+++ students/chankyu/users/views.py
@@ -1,7 +1,7 @@
 import json, re, bcrypt, jwt
 
 from django.shortcuts import render
-from django.http.response import HttpResponse, JsonResponse
+from django.http.response import JsonResponse
 from django.views import View
 
 from users.models import User
@@ -19,13 +19,13 @@ class SignupView(View):
             age          = data['age']
 
             if not re.match('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', email):
-                return HttpResponse({"MESSAGE" : "INVALID_EMAIL"}, status=403)
+                return JsonResponse({"MESSAGE" : "INVALID_EMAIL"}, status=403)
 
             if not re.match('^(?=.*[A-Za-z])(?=.*\d)(?=.*[?!@#$%*&])[A-Za-z\d?!@#$%*&]{8,}$', password):
-                return HttpResponse({"MESSAGE" : "INVALID_PASSWORD"}, status=403)
+                return JsonResponse({"MESSAGE" : "INVALID_PASSWORD"}, status=403)
 
             if User.objects.filter(email=email).exists():
-                return HttpResponse({"MESSAGE" : "ALREADY EXISTED"}, status=409)
+                return JsonResponse({"MESSAGE" : "ALREADY EXISTED"}, status=409)
 
             bcrypt_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
             
@@ -36,10 +36,10 @@ class SignupView(View):
                 phone_number = phone_number,
                 age          = age
             )
-            return HttpResponse({"MESSAGE" : "SUCCESS"}, status = 201)
+            return JsonResponse({"MESSAGE" : "SUCCESS"}, status = 201)
 
         except:
-            return HttpResponse({"MESSAGE" : "KEY_ERROR"}, status = 400)
+            return JsonResponse({"MESSAGE" : "KEY_ERROR"}, status = 400)
 
 class SigninView(View):
     def post(self, request):
@@ -53,11 +53,11 @@ class SigninView(View):
 
             if bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
                 jwt_token = jwt.encode({'id':user.id}, SECRET_KEY, algorithm = ALGORITHM)
-                return HttpResponse({'ACCESS_TOKEN' : jwt_token},{'MESSAGE':'SUCCESS'}, status = 200)
+                return JsonResponse({'ACCESS_TOKEN' : jwt_token}, status = 200)
             else:
-                return HttpResponse({'MESSAGE':'WRONG_PASSWORD'}, status = 401)
+                return JsonResponse({'MESSAGE':'WRONG_PASSWORD'}, status = 401)
 
         except KeyError:
-            return HttpResponse({"MESSAGE" : "KEY_ERROR"}, status = 400)
+            return JsonResponse({"MESSAGE" : "KEY_ERROR"}, status = 400)
         except User.DoesNotExist:
-            return HttpResponse({"MESSAGE":"NOT_EXISTED_USER"}, status = 401)
+            return JsonResponse({"MESSAGE":"NOT_EXISTED_USER"}, status = 401)
