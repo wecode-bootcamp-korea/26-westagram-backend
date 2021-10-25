93069a27cffa301a904fdf412aa5f511dfc17c4f
kimbongcheol <solarrrrr1010@gmail.com>
2021-10-25 12:32:58 +0900

    회원가입 리뷰 반영

--- students/bong/users/urls.py
+++ students/bong/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UserView
+from .views import UserView #, LoginView
 
 urlpatterns = [
     path("User", UserView.as_view()),
+    #path("User", LoginView.as_view()),
 ]
--- students/bong/users/views.py
+++ students/bong/users/views.py
@@ -14,16 +14,14 @@ class UserView(View):
         email               = data["email"]
         password            = data["password"]
         phone_num           = data["phone_num"]
-        email_validation    = re.compile(r'^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
-        password_validation = re.compile(r'^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[~₩!@#$%^&*()\-_=+])[a-zA-Z0-9~!@#$%^&*()_\-+=]{8,}$')
 
-        if email == "" or password == "":
+        if not (email and password):
             return JsonResponse({"message" : "KEY_ERROR"}, status=400)
     
-        if not email_validation.match(email):
+        if not re.match('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', email):
             return JsonResponse({"message" : "EMAIL_VALIDATION_ERROR"}, status=400)
 
-        if not password_validation.match(password):
+        if not re.match('^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[~₩!@#$%^&*()\-_=+])[a-zA-Z0-9~!@#$%^&*()_\-+=]{8,}$', password):
             return JsonResponse({"message" : "PASSWORD_VALIDATION_ERROR"}, status=400)
 
         if User.objects.filter(email=email).exists():
@@ -34,8 +32,8 @@ class UserView(View):
             email=data["email"],
             password=data["password"],
             phone_num=data["phone_num"],
-            #created_at=data["created_at"],
-            #updated_at=data["updated_at"],
+     
             )
         
         return JsonResponse({"message" : "SUCCESS"}, status=201)
+
