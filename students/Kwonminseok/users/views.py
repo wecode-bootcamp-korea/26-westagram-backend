a511e4afd7baf3dd3cb6296d1b3861498348d02d
knch1014 <knch1014@gmail.com>
2021-10-25 15:24:56 +0900

    수정사항 반영

--- students/Kwonminseok/users/views.py
+++ students/Kwonminseok/users/views.py
@@ -8,26 +8,29 @@ from django.core.exceptions import ValidationError
 from .models import User
 
 class SignUpView(View):
-    def post(self, request):
-        data           = json.loads(request.body)
-        regex_email    = re.compile('^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
-        regex_password = re.compile("^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$")
-       
+    def post(self, request):       
         try:
-            if data["email"] == "" or data["password"] == "":
+            data           = json.loads(request.body)
+            name           = data["name"]
+            email          = data["email"]
+            password       = data["password"]
+            phone_number   = data["phone_number"]
+            age            = data["age"]
+ 
+            if not (password and email):
                 return JsonResponse({'message' : 'NO_VALUE'}, status=400)
-            if not regex_email.match(data["email"]):
+            if not re.match('^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', email):
                 return JsonResponse({'message' : 'EMAIL_VALIDATION_ERROR'}, status=400)
-            if not regex_password.match(data["password"]):
+            if not re.match("^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$", password):
                 return JsonResponse({'message' : 'PASSWORD_VALIDATION_ERROR'}, status=400)
-            if User.objects.filter(email=data["email"]).exists():
+            if User.objects.filter(email=email).exists():
                 return JsonResponse({'message' : 'DUPLICATED EMAIL'}, status=400)
             User.objects.create(
-                name         = data["name"],
-                email        = data["email"],
-                password     = data["password"],
-                phone_number = data["phone_number"],
-                age          = data["age"]
+                name         = name,
+                email        = email,
+                password     = password,
+                phone_number = phone_number,
+                age          = age
             )
             return JsonResponse({'message' : 'SUCCESS'}, status=201)
         except KeyError:
