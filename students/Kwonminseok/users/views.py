f273b02f90e96ad0ef4c03699109f7f901ae6f5c
knch1014 <knch1014@gmail.com>
2021-10-25 13:30:21 +0900

    [권민석] Mission3 회원가입 기능 구현

--- students/Kwonminseok/users/views.py
+++ students/Kwonminseok/users/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+import json
+import re
 
-# Create your views here.
+from django.views           import View
+from django.http            import JsonResponse, HttpResponse
+from django.core.exceptions import ValidationError
+
+from .models import User
+
+class SignUpView(View):
+    def post(self, request):
+        data           = json.loads(request.body)
+        regex_email    = re.compile('^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
+        regex_password = re.compile("^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$")
+       
+        try:
+            if data["email"] == "" or data["password"] == "":
+                return JsonResponse({'message' : 'NO_VALUE'}, status=400)
+            if not regex_email.match(data["email"]):
+                return JsonResponse({'message' : 'EMAIL_VALIDATION_ERROR'}, status=400)
+            if not regex_password.match(data["password"]):
+                return JsonResponse({'message' : 'PASSWORD_VALIDATION_ERROR'}, status=400)
+            if User.objects.filter(email=data["email"]).exists():
+                return JsonResponse({'message' : 'DUPLICATED EMAIL'}, status=400)
+            User.objects.create(
+                name         = data["name"],
+                email        = data["email"],
+                password     = data["password"],
+                phone_number = data["phone_number"],
+                age          = data["age"]
+            )
+            return JsonResponse({'message' : 'SUCCESS'}, status=201)
+        except KeyError:
+            return JsonResponse({'message' : 'KEY_ERROR'}, status=400)         
+       
+             
