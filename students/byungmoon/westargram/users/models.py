82fb1f04cea3df2f00d1588bc558e1ca20843962
YUBYUNGMOON <qudans1237@gmail.com>
2021-10-25 19:48:03 +0900

    Add:products application

--- students/byungmoon/westargram/users/models.py
+++ students/byungmoon/westargram/users/models.py
@@ -3,10 +3,12 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     name = models.CharField(max_length=100)
-    email = models.CharField(max_length=200)
-    password = models.CharField(max_length=100)
+    email = models.EmailField(max_length=200)
+    password = models.CharField(max_length=200)
     contact = models.CharField(max_length=100)
-    OtherPersonalInf = models.CharField(max_length=200)
-
+    Other_Personal_Inf = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
     class Meta:
         db_table = "users"
