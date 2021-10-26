e912d27329edd6f76ea1146423190b4b8fe05676
Minjeong Bak <doniminp@gmail.com>
2021-10-26 14:06:48 +0900

    login view setting

--- students/minjeong/users/urls.py
+++ students/minjeong/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views      import UserView
+from .views      import UserView, LoginView
 
 urlpatterns = [
     path('/signup', UserView.as_view()),
+    path('/login', LoginView.as_view())
 ]
--- students/minjeong/users/views.py
+++ students/minjeong/users/views.py
@@ -33,10 +33,33 @@ class UserView(View):
                 personal_info = personal_info
             )
 
-            return JsonResponse({"message" : "SUCCESS"}, status = 201)
+            return JsonResponse({'message' : 'SUCCESS'}, status = 201)
 
         except KeyError:
-            return JsonResponse({"message" : "KEY_ERROR"}, status = 400)
+            return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)
 
+class LoginView(View):
+    def post(slef, request):
 
+        try:
+            data = json.loads(request.body)
+            email         = data['email']
+            password      = data['password']
 
+            # 계정을 잘못 입력한 경우 -> {'message' : 'INVALID_USER'}, status code 401
+            if not User.objects.filter(email=email).exists():
+                return JsonResponse({'message' : 'INVALID_USER'}, status = 401)
+            # 비밀번호를 잘못 입력한 경우 -> {'message' : 'INVALID_USER'}, status code 401
+            if not User.objects.filter(password=password).exists():
+                return JsonResponse({'message' : 'INVALID_USER'}, status = 401)
+            
+            User.objects.get(
+                email    = email,
+                password = password
+            )
+
+            # 로그인 성공
+            return JsonResponse({'message' : 'SUCCESS'}, status = 200)
+            # 계정이나 패스워드 키가 전달되지 않았을 경우 -> {'message' : 'KEY_ERROR'}, status code 400
+        except KeyError:
+            return JsonResponse({'message': 'KEY_ERROR'}, status = 400)
