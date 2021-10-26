2c9e5e9ec60a11141b94fcb90c26b4e6435a5dad
1119 pdh <1119pdh@gmail.com>
2021-10-26 11:30:36 +0900

    project sign-up

--- students/parkdaehyun/users/urls.py
+++ students/parkdaehyun/users/urls.py
@@ -1,8 +1,7 @@
 from django.urls               import path
-from django.urls.resolvers import URLPattern
+from django.urls.resolvers     import URLPattern
 from django.views.generic.base import View
 from .views                    import UserlistView
-
 
 urlpatterns = [
     path("user", UserlistView.as_view())
