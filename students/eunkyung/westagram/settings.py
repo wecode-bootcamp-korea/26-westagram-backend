73491002ae18a297b42bc6eaf4601d8a47d5a4e9
권은경 <kwon-eunkyung@gwon-eungyeong-ui-MacBookAir.local>
2021-10-26 14:37:07 +0900

    ADD:회원가입기능 구현

--- students/eunkyung/westagram/settings.py
+++ students/eunkyung/westagram/settings.py
@@ -26,7 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-20om$i&ict#5!m$%p8vy12=%3cgo@hke@^-d7juhwq&j(@h%3+'
+DATABASES = DATABASES
+
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,18 +79,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'westagram.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
