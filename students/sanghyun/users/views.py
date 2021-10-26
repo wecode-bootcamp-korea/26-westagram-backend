58941c52c06b968f3ff690d90d08e97de1f30c16
gshduet <modis2199@gmail.com>
2021-10-26 12:38:23 +0900

    ADD : 로그인 기능 구현

--- students/sanghyun/users/views.py
+++ students/sanghyun/users/views.py
@@ -34,3 +34,17 @@ class SignUpView(View):
             return JsonResponse({'MESSAGE':'CREATED'}, status=201)
         except KeyError:
             return JsonResponse({"MESSAGE" : "KEY_ERROR"}, status = 400) 
+
+class LogInView(View):
+    def post(self, request):
+        try:
+            data        = json.loads(request.body)
+            email       = data['email']
+            password    = data['password']
+
+            if not User.objects.filter(email=email, password=password).exists():
+                return JsonResponse({"message": "INVALID_USER"}, status=401)
+
+            return JsonResponse({"message": "SUCCESS"}, status=200)
+        except KeyError:
+            return JsonResponse({"MESSAGE" : "KEY_ERROR"}, status = 400) 
