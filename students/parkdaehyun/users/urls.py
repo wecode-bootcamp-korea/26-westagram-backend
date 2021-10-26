3942844a60ab6aef8aa0c31fe32dc24febd0748b
1119 pdh <1119pdh@gmail.com>
2021-10-26 18:57:26 +0900

    project login

--- students/parkdaehyun/users/urls.py
+++ students/parkdaehyun/users/urls.py
@@ -3,6 +3,6 @@ from django.urls               import path
 from .views                    import Login, UserlistView
 
 urlpatterns = [
-    path("user", UserlistView.as_view()),
+    path("/user", UserlistView.as_view()),
     path("/login", Login.as_view())
 ]
