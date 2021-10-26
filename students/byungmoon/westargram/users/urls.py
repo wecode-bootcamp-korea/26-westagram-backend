9fda601ac355f81e8f2b161de57fcd460908affc
YUBYUNGMOON <qudans1237@gmail.com>
2021-10-26 18:03:21 +0900

    Add: products application

--- students/byungmoon/westargram/users/urls.py
+++ students/byungmoon/westargram/users/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 
 from django.urls import path, include
+
 from .views      import SignUpView, SignInView
 
 urlpatterns = [
 	path('/signup', SignUpView.as_view()),
     path('/signin', SignInView.as_view()),
-] 
+]
