9a9b7dfa8fce54b916a88eba843eda669e72cbfd
Minjeong Bak <doniminp@gmail.com>
2021-10-27 21:30:24 +0900

    JWT setting

--- students/minjeong/users/views.py
+++ students/minjeong/users/views.py
@@ -1,20 +1,21 @@
-import json, re, bcrypt
+import json, re, bcrypt, jwt
 
 from django.http    import JsonResponse
 from django.views   import View
 
-from .models   import User
+from .models     import User
+from django.conf import settings
 
 class UserView(View):
     def post(self, request):        
 
         try:
             data = json.loads(request.body)
-            name          = data['name']
+            name          = "임기범"
             email         = data['email']
             password      = data['password']
-            phone_number  = data['phone_number']
-            personal_info = data['personal_info']
+            phone_number  = "010-2222-2222"
+            personal_info = "ㅇㅇ"
 
             if not re.match('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', email):
                 return JsonResponse({'message':'EMAIL_NOT_VALID'}, status=400)
@@ -25,12 +26,12 @@ class UserView(View):
             if User.objects.filter(email=email).exists():
                 return JsonResponse({'message' : 'SAME_EMAIL_ERROR'}, status = 400)
 
-            hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+            hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
             User.objects.create(
                 name          = name,
                 email         = email,
-                password      = hashed_password.decode('utf-8'),
+                password      = hashed_password,
                 phone_number  = phone_number,
                 personal_info = personal_info
             )
@@ -47,11 +48,16 @@ class LoginView(View):
             data = json.loads(request.body)
             email         = data['email']
             password      = data['password']
+            user          = User.objects.get(email=email)
+            encode_jwt =jwt.encode({'user_id' : user.id}, settings.SECRET_KEY, settings.ALGORITHM)
 
-            if not User.objects.filter(email=email, password=password).exists():
-                return JsonResponse({'message' : 'INVALID_USER'}, status = 401)            
-
-            return JsonResponse({'message' : 'SUCCESS'}, status = 200)
+            if bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
+                return JsonResponse({'message' : 'SUCCESS', 'jwt' : encode_jwt}, status = 200)
+            else:
+                return JsonResponse({'message' : 'INVALID_USER'}, status = 400)
 
         except KeyError:
             return JsonResponse({'message': 'KEY_ERROR'}, status = 400)
+        except User.DoesNotExist :
+            return JsonResponse({"message" : "Unauthorized"}, status = 401) 
+
