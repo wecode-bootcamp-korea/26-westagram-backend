4fc1fe40be136f7768b8d1f6dff75ebf6903047c
권은경 <kwon-eunkyung@gwon-eungyeong-ui-MacBookAir.local>
2021-10-28 13:43:18 +0900

    ADD: ACCESS TOKEN

--- students/eunkyung/users/views.py
+++ students/eunkyung/users/views.py
@@ -1,7 +1,9 @@
-import json, bcrypt
+import json, bcrypt, jwt
 
 from django.http    import JsonResponse
 from django.views   import View
+
+from django.conf import settings
 
 from .models        import User
 from .utils         import validate_email, vaildate_password
@@ -43,14 +45,21 @@ class SignupView(View):
 class LoginView(View):
     def post(self, request):
         data = json.loads(request.body)
+
         try :
             email    = data['email']
             password = data['password']
+            user     = User.objects.get(email=email)
 
-            if not User.objects.filter(email=email, password=password).exists():
-                return JsonResponse({"MESSAGE":"INVALID_USER"}, status=401)
-                
-            return JsonResponse({'MESSAGE':'SUCCESS'}, status=200)
+            if bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
+                access_token = jwt.encode({'UserID': user.id}, settings.SECRET_KEY,algorithm=settings.ALGORITHM ).decode('utf-8')
+                return JsonResponse({'ACCESS_TOKEN': access_token}, status=200)
+
+            return JsonResponse({'MESSAGE':'INVALID_USER'}, status=401)
 
         except KeyError:
             return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)
+
+        except User.DoesNotExist :
+            return JsonResponse({'MASSAGE':'USER_DOES_NOT_EXISTS'}, status = 401) 
+            
