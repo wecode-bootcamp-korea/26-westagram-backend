1dd71548152ed1180f6fa7343392bdedd0214ed5
kimbongcheol <solarrrrr1010@gmail.com>
2021-10-28 12:32:45 +0900

    jwt 토큰 발행 2

--- students/bong/users/views.py
+++ students/bong/users/views.py
@@ -4,17 +4,19 @@ import bcrypt, jwt
 
 from django.views import View
 from django.http import HttpResponse, JsonResponse
+from django.conf.settings import ALGORITHM, SECRET_KEY
 
 from .models import User
-from my_settings import SECRET_KEY
+
 
 class SignupView(View):
     def post(self, request):
         data        = json.loads(request.body)
-        name        = data.get("name")
-        email       = data.get("email")
-        password    = data.get("password")
-        phone_num   = data.get("phone_num")
+        print(data)
+        name        = data["name"]
+        email       = data["email"]
+        password    = data["password"]
+        phone_num   = data["phone_num"]
 
         if not (email and password):
             return JsonResponse({"message" : "KEY_ERROR"}, status=400)
@@ -24,13 +26,17 @@ class SignupView(View):
 
         if not re.match('^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[~₩!@#$%^&*()\-_=+])[a-zA-Z0-9~!@#$%^&*()_\-+=]{8,}$', password):
             return JsonResponse({"message" : "PASSWORD_VALIDATION_ERROR"}, status=400)
-
+        
         if User.objects.filter(email=email).exists():
             return JsonResponse({"message" : "DUPLICATION_ERROR"}, status=400)
 
+        hashed_password = bcrypt.hashpw(data["password"].encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+
         User.objects.create(
-            name, email, phone_num,
-            password    = (bcrypt.hashpw(data.get("password").encode('utf-8'), bcrypt.gensalt())).decode('utf-8'),
+            email       = email, 
+            password    = hashed_password,
+            name        = name,
+            phone_num   = phone_num
             )
         
         return JsonResponse({"message" : "SUCCESS"}, status=200)
@@ -38,16 +44,17 @@ class SignupView(View):
 class LoginView(View):
     def post(self, request):
         data            = json.loads(request.body)
-        email           = data.get("email")
-        password        = data.get("password")
+        email           = data["email"]
+        password        = data["password"]
         
-        if not (email and password):
-            return JsonResponse({"messgae" : "KEY_ERROR"}, status=400)
-       
-        if User.objects.filter(email=email).exists():
+        try:
             user = User.objects.get(email=email)
+
             if bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
-                token = jwt.encode({"email" : email}, SECRET_KEY, algorithm='HS256')
+                token = jwt.encode({"email" : email}, SECRET_KEY, algorithm=ALGORITHM)
                 return JsonResponse({"TOKEN" : token}, status=200)
                 
-        return JsonResponse({"message" : "INVALID_USER"}, status=401)            
+        except User.DoesNotExist:
+            return JsonResponse({"message" : "USER_DOES_EXISTS"}, status=401) 
+        
+  
